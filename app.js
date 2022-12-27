20e40082f23e9917662e30fba75935fd7de7b1c3
Ash Moreno <ashterfuentes@gmail.com>
2022-12-27 17:31:22 -0600

    feat: require fs

--- app.js
+++ app.js
@@ -6,26 +6,19 @@ form.addEventListener("submit", (event) => {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
 
-  console.log(name);
-  console.log(email);
-
   // store the name and email in the data.js file
   storeData(name, email);
 });
 
-const fs = require("fs");
+import { writeFileSync } from "fs";
 
 function storeData(name, email) {
   // create an object with the name and email
   const data = { name, email };
 
-  console.log(data);
-
   // convert the object to a JSON string
   const dataString = JSON.stringify(data);
 
-  console.log(dataString);
-
   // write the JSON string to the data.js file
-  fs.writeFileSync("data.js", dataString);
+  writeFileSync("data.js", dataString);
 }
